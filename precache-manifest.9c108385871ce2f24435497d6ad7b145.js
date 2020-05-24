76ee2cc98856841db466aba94c69e41dee4aefb8
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2020-05-24 16:59:49 +0100

    Updates

---
+++ precache-manifest.9c108385871ce2f24435497d6ad7b145.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918aac7f07cd0ba5eba0c39ae9499b1a",
+    "url": "/automaton-simulator/index.html"
+  },
+  {
+    "revision": "31f7070197135f2c2663",
+    "url": "/automaton-simulator/static/css/main.1bbd5e63.chunk.css"
+  },
+  {
+    "revision": "96521d84b70bcf90b2de",
+    "url": "/automaton-simulator/static/js/2.606ec056.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/automaton-simulator/static/js/2.606ec056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f7070197135f2c2663",
+    "url": "/automaton-simulator/static/js/main.a5beb123.chunk.js"
+  },
+  {
+    "revision": "225cd87c69db15d2387c",
+    "url": "/automaton-simulator/static/js/runtime-main.ee58ba5b.js"
+  },
+  {
+    "revision": "4f64643f69fb440c4629ab1048b5a495",
+    "url": "/automaton-simulator/static/media/mononoki-regular.4f64643f.woff"
+  }
+]);
